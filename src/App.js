a80517eeac4bb1c56931bf2fd4729a30a8bcf5a9
trader codes <waozin100@gmail.com>
2022-05-07 14:58:49 -0700

    done

--- src/App.js
+++ src/App.js
@@ -2,7 +2,17 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+   const [people, setPeople] = useState(data);
+   return (
+      <main>
+         <section className="container">
+            <List people={people} />
+            {/* people,length check how many people are there  */}
+            <h3>{people.length} birthdays today</h3>
+            <button onClick={() => setPeople([])}>clear all</button>
+         </section>
+      </main>
+   );
 }
 
 export default App;
